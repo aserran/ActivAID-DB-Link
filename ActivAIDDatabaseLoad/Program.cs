e31d299d7af167d7993cface0a063923d7a53242
Alfonso <alfonso@promenadesoftware.com>
2017-06-19 15:42:02 -0700

    Update to DB loader: hardcoded server name to localhost/SQLSERVER

--- ActivAIDDatabaseLoad/Program.cs
+++ ActivAIDDatabaseLoad/Program.cs
@@ -13,7 +13,7 @@ namespace ActivAID
     {
         static void Main(string[] args)
         {
-            List<string> filePaths = DirectoryLoader.makeFileList("C:\\Program Files\\ActivATE\\ActivATE 5.x\\ActivAID\\help\\Html");
+            List<string> filePaths = DirectoryLoader.makeFileList("C:\\Program Files\\ActivATE\\ActivATE 5.x\\ActivAID\\Debug\\Media\\HelpHTML");
             ParserWrapper pwrap = new ParserWrapper(filePaths);
         }
     }
